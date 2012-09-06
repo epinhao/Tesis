7575f55fa53ee632c19adb226d3582d68da3252b
Emilio <Emilio@192.168.1.85>
2012-09-06 00:10:14 -0500

    Test upstream

--- OrderBook/src/SQLReadSingle.java
+++ OrderBook/src/SQLReadSingle.java
@@ -30,7 +30,7 @@ public class SQLReadSingle {
 		Sql reader = new Sql();
 		final String emisora = "BIMBO";
 		final String serie = "";
-		final Calendar cal = new GregorianCalendar(2010,11,13);
+		final Calendar cal = new GregorianCalendar(2010,11,14);
 		ArrayList<Orden> ordenes = reader.readIniciales(emisora, serie, cal);
 		reader.read(ordenes, emisora, serie, cal);
 
