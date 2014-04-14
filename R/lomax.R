9e28e9355bd12bbf4944c50f9f2c79191a7207d4
Emilio <Emilio@192.168.1.85>
2014-04-13 21:24:47 -0500

    Industrial 2

--- R/lomax.R
+++ R/lomax.R
@@ -3,6 +3,10 @@ y=runif(n)
 x = -l *((1-y)^(1/a)-1)* (1-y)^(-1/a)
 return(x)
 }
+qlomax <- function(l,a,y) {
+x = -l *((1-y)^(1/a)-1)* (1-y)^(-1/a)
+return(x)
+}
 plomax <- function(x,l,a) {
 	1-(1+x/l)^(-a)
 }
